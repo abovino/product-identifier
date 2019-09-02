1eac37ff519bcee48792f4d413730d9421f40b36
Angelo Bovino <angelo.bovino@outlook.com>
2019-09-02 18:50:41 -0400

    Product class for holding attributes related to product being searched

---
+++ product-identifer/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace product_identifer
+{
+    class Product
+    {
+        public string materialNo { get; private set; }
+
+        public Product(string materialNo)
+        {
+            this.materialNo = materialNo;
+        }
+    }
+}
