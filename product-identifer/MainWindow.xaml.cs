e925bd624cb96f9da48967b926a6ba9b72eb2348
Angelo Bovino <angelo.bovino@outlook.com>
2019-09-04 21:00:47 -0400

    ProductInfo query

--- product-identifer/MainWindow.xaml.cs
+++ product-identifer/MainWindow.xaml.cs
@@ -28,8 +28,27 @@ namespace product_identifer
         private void handleSearch(object sender, RoutedEventArgs e)
         {
             var upc = upcTxt.Text;
-            
-            
+
+            using (var context = new ProductsModel())
+            {
+                var productInfo = (from products in context.products
+                                   join productImgs in context.product_images
+                                     on products.material_no equals productImgs.material_no
+                                   join productHierarchies in context.product_hierarchies
+                                     on products.hierarchy_id equals productHierarchies.hierarchy_id
+                                   where products.upc == upc
+                                   select new
+                                   {
+                                       products.material_no,
+                                       products.upc,
+                                       products.description,
+                                       products.uom,
+                                       products.hierarchy_id,
+                                       productHierarchies.hierarchy_description,
+                                       productImgs.file_name,
+                                   }).FirstOrDefault();
+
+            }
         }
     }
 }
