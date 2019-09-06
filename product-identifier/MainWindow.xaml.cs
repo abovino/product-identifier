57876020afbb98c1008edd99cc07476091d57838
Angelo Bovino <angelo.bovino@outlook.com>
2019-09-05 22:29:34 -0400

    GetProductInfo and GetProductInventoryByPlant methods to separate queries and make each query more reuseable

--- product-identifier/MainWindow.xaml.cs
+++ product-identifier/MainWindow.xaml.cs
@@ -30,15 +30,18 @@ namespace product_identifer
             var upc = upcTxt.Text;
 
             Product product = new Product();
+            List<Inventory> inventory = new List<Inventory>();
 
             using (var context = new ProductsModel())
             {
-                product = context.Products.Where(p => p.upc == upc).SingleOrDefault();
+                product = GetProductInfo(context, upc);
+                inventory = GetProductInventoryByPlant(context, product.material_no, "7010");
 
                 if (product != null)
                 {
                     // Update UI
                     UpdateUi(product);
+                    UpdateStockGrid(inventory, "plant");
                 }
                 else
                 {
@@ -48,13 +51,36 @@ namespace product_identifer
             
         }
 
+        public Product GetProductInfo(ProductsModel context, string upc)
+        {
+            Product product = new Product();
+            product = context.Products.Where(p => p.upc == upc).SingleOrDefault();
+
+            return product;
+        }
+
+        public List<Inventory> GetProductInventoryByPlant(ProductsModel context, string materialNo, string plantId)
+        {
+            List<Inventory> inventory = new List<Inventory>();
+            inventory = context.Inventories
+                .Where(
+                    i => (i.material_no == materialNo) && (i.plant_id == plantId)
+                 ).ToList();
+
+            return inventory;
+        }
+
         public void UpdateUi(Product product)
         {
             this.descriptionTxt.Text = product.description;
             this.hierarchyIdTxt.Text = product.hierarchy_id;
             this.hierarchyTxt.Text = product.product_hierarchies.hierarchy_description;
             this.imgContainer.Source = new BitmapImage(new Uri($"H:\\C#\\product-identifier\\product-identifier\\Resources\\{product.product_images.file_name}", UriKind.Absolute));
+        }
 
+        public void UpdateStockGrid(List<Inventory> inventory, string plant)
+        {
+            MessageBox.Show(plant);
         }
     }
 }
